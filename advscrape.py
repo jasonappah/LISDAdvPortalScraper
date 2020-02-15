5028cec846f0299916c3694aedd1139f5f3ee2e9
Jason Antwi-Appah <jasonantwiappah@gmail.com>
2020-02-15 15:04:46 -0600

    Finished the file selection / argument parser
    I found out argparse was a thing after I did all this. :( It do be like that sometimes

--- advscrape.py
+++ advscrape.py
@@ -1,5 +1,37 @@
-import sys
+import os, sys
+from lxml import etree
+
+def scrape(filename):
+    print("Scraping " + filename)
+    parse = etree.HTMLParser()
+    tree = etree.parse(filename, parse)
+    print(str(tree))
+
 
 if len(sys.argv) == 1:
-    print("Usage: \n advscrape htmlfile")
+    files = os.scandir()
+    scrapable = []
+    for file in files:
+        if "lADV-" in file.name:
+            scrapable.append(file.name)
 
+    print("Choose an Advocate Portal File by number:")
+    for file in scrapable:
+        i=0
+        print("[" + str(i) + "]" + ": "+ str(scrapable[i]))
+        i+=1
+
+    choice = input("Enter the number only: ")
+    choice = int(choice)
+    scrape(str(scrapable[choice]))
+
+elif len(sys.argv) == 2:
+    print (str(len(sys.argv)) + " arguments detected")
+    for i in sys.argv:
+        print (str(i) + " " + str(type(i)))
+
+    if "lADV-" in sys.argv[1]:
+        scrape(str(sys.argv[1]))
+    
+else:
+    print("Usage: \n advscrape \n advscrape pathtohtmlfile")
